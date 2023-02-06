8a953a5cd7d5f419aa91b4d942dc8e9bce7df575
Rio <onwukachibike@gmail.com>
2023-02-06 15:17:48 +0100

    updated

---
+++ feedbackweb3/feedbackBackend/hardhat.config.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+    goerli: {
+      url: process.env.GOERLI_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+   
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.POLYGONSCAN_API_KEY
+    }
+  }
+};
