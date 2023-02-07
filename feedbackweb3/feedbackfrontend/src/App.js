9c12d642ed0042737c902f468432be934a165920
Rio <onwukachibike@gmail.com>
2023-02-07 16:01:39 +0100

    updated the ui

--- feedbackweb3/feedbackfrontend/src/App.js
+++ feedbackweb3/feedbackfrontend/src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
+import feedbackAbi from "./utils/feedbackabi.json"
 {/* <img src={logo} className="App-logo" alt="logo" /> */}
 
 
-
+let abi = feedbackAbi.abi;
+console.log(abi)
 
 function App() {
   return (
     <div className="App">
       <Nav/>
+      
     </div>
   );
 }
--- feedbackweb3/feedbackfrontend/src/components/Nav.js
+++ feedbackweb3/feedbackfrontend/src/components/Nav.js
@@ -14,9 +14,9 @@ const Nav = () => {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
        
-        <li>
+        <button>
           <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Connect Wallet</a>
-        </li>
+        </button>
       </ul>
     </div>
   </div>
