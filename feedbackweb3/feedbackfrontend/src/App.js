f3302829cc433aeae71bc9ee817d59b1ea53afac
Rio <onwukachibike@gmail.com>
2023-02-07 16:33:41 +0100

    updated ui

--- feedbackweb3/feedbackfrontend/src/App.js
+++ feedbackweb3/feedbackfrontend/src/App.js
@@ -6,13 +6,24 @@ import feedbackAbi from "./utils/feedbackabi.json"
 
 
 let abi = feedbackAbi.abi;
-console.log(abi)
+
 
 function App() {
   return (
     <div className="App">
       <Nav/>
-      
+      <h1 className='text-3xl font-bold underline'>Feedbacks Received</h1>
+      <div className='flex items-center  justify-center my-10 '>
+       <div className='border-2 rounded-sm w-1/2'>
+       <p className='text-xl font-bold'>"You worked to the end senior"</p>
+        <p className='text-gray-800 '>from: <span className='text-xl font-bold'>Onwuka Rosario</span></p>
+       </div>
+      </div>
+
+      <button className='mt-10 bg-blue-400 rounded-md py-2 px-5 text-white font-bold'>
+      Like my work? send eth to me ;) <br/>
+      just 0.001 eth
+      </button>
     </div>
   );
 }
