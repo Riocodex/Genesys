60bc47757abf139c9945cc156069a52185f42821
Rio <onwukachibike@gmail.com>
2023-02-08 13:55:13 +0100

    updated connect wallet button

--- feedbackweb3/feedbackfrontend/src/App.js
+++ feedbackweb3/feedbackfrontend/src/App.js
@@ -122,8 +122,8 @@ function App() {
     <div class="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
        
-        <button>
-          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Connect Wallet</a>
+        <button onClick={connectWallet} className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>Connect Wallet
+          
         </button>
       </ul>
     </div>
