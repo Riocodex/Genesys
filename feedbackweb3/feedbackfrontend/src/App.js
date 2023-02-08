69c129f9886ae5b9279eb2e1a55cc1b88d107649
Rio <onwukachibike@gmail.com>
2023-02-08 13:49:44 +0100

    added wallet connection logic

--- feedbackweb3/feedbackfrontend/src/App.js
+++ feedbackweb3/feedbackfrontend/src/App.js
@@ -29,6 +29,45 @@ function App() {
     setMessage(event.target.value);
   }
 
+
+  // Wallet connection logic
+  const isWalletConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      const accounts = await ethereum.request({method: 'eth_accounts'})
+      console.log("accounts: ", accounts);
+
+      if (accounts.length > 0) {
+        const account = accounts[0];
+        console.log("wallet is connected! " + account);
+      } else {
+        console.log("make sure MetaMask is connected");
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const {ethereum} = window;
+
+      if (!ethereum) {
+        console.log("please install MetaMask");
+      }
+
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts'
+      });
+
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  
   return (
     <div className="App">
       
