a9534cae52c202b4dadb13e2228c31d07ff00708
Rio <onwukachibike@gmail.com>
2023-02-08 13:48:09 +0100

    added nav

--- feedbackweb3/feedbackfrontend/src/App.js
+++ feedbackweb3/feedbackfrontend/src/App.js
@@ -28,7 +28,25 @@ function App() {
 
   return (
     <div className="App">
-      <Nav/>
+      
+      <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+  <div class="container flex flex-wrap items-center justify-between mx-auto">
+    <a href="https://riocodex.vercel.app/" class="flex items-center">
+        <img src={rio} class="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">RioCodeX</span>
+    </a>
+ 
+    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul class="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+       
+        <button>
+          <a href="#" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Connect Wallet</a>
+        </button>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 
      <div className="flex items-center justify-center ">
      <form className = "bg-blue-400 shadow-md w-1/2 mt-5 rounded px-8 pt-6 pb-8 mb-2">
